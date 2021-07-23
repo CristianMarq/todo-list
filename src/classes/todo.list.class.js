2d91b17ebfc983db3334b51b5b7dd105ec1715a1
Christian Marquez <marquezcristian920@gmail.com>
2021-07-22 21:31:51 -0600

    Recuperando y persistiendo datos en el local Storage

--- src/classes/todo.list.class.js
+++ src/classes/todo.list.class.js
@@ -1,24 +1,28 @@
 import { todoList } from "..";
 
 export class TodoList {
-  _todos;
+  // _todos;
 
   constructor() {
-    this._todos = [];
+    // this._todos = [];
+    this.cargarLocalStorage();
   }
 
   nuevoTodo(todo) {
     this._todos.push(todo);
+    this.guardarLocalStorage();
   }
 
   eliminarTodo(id) {
     this._todos = this._todos.filter((todo) => todo._id != id);
+    this.guardarLocalStorage();
   }
 
   marcarCompletado(id) {
     for (const todo of this._todos) {
       if (todo._id == id) {
         todo._completado = !todo._completado;
+        this.guardarLocalStorage();
         break;
       }
     }
@@ -26,5 +30,16 @@ export class TodoList {
 
   eliminarCompletados() {
     this._todos = this._todos.filter((todo) => !todo._completado);
+    this.guardarLocalStorage();
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem("todo", JSON.stringify(this._todos));
+  }
+
+  cargarLocalStorage() {
+    this._todos = localStorage.getItem("todo")
+      ? JSON.parse(localStorage.getItem("todo"))
+      : [];
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import { TodoClass, TodoList } from "./classes";
 import { crearTodoHtml } from "./js/componentes";
 
 export const todoList = new TodoList();
-const todo = new TodoClass("Aprender react Js");
 
-todoList.nuevoTodo(todo);
-
-console.log(todoList);
-
-crearTodoHtml(todo);
+todoList._todos.forEach((element) => crearTodoHtml(element));
